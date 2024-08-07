8262c9c9d92b9748afa59527fb9426a93b46f65a
Miguel Obando <miguel.obando204@gmail.com>
2024-08-07 13:06:39 -0500

    feature: Welcome page

---
+++ src/components/ActionBtn.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="btn" @click="clickFn">{{ msg }}</button>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{
+  msg: string
+  clickFn: (event: MouseEvent) => void
+}>()
+</script>
+
+<style scoped>
+.btn {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0.6875rem 1.25rem;
+  gap: 0.625rem;
+  font-family: 'Lato';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.125rem;
+  line-height: 1.375rem;
+  background: #f22539;
+  border-radius: 3.75rem;
+  color: white;
+  border: none;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,104 @@
+<template>
+  <div class="pokedex-container">
+    <div class="img-container">
+      <img class="pika-img" src="../assets/image.png" alt="pokedex" />
+    </div>
+    <h2 class="welcome-header">Welcome to the Pokedex</h2>
+    <div class="pokedex-description">
+      <p>The digital encyclopedia created by Professor Oak is an invaluable tool</p>
+      <p>to Trainers in the Pokemon world.</p>
+    </div>
+    <ActionBtn msg="Get Started" :clickFn="goToDashboard" />
+  </div>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import ActionBtn from '../components/ActionBtn.vue'
+const goToDashboard = () => {
+  alert('Go to Dashboard')
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+.pokedex-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: aliceblue;
+  padding: 2rem;
+  width: 100%;
+  height: 100vh; /* Ensure it takes the full viewport height */
+  box-sizing: border-box;
+  overflow: hidden; /* Prevent scrolling */
+}
+
+.img-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 1.5rem;
+}
+
+.pika-img {
+  max-width: 30%;
+  height: auto;
+}
+
+.welcome-header {
+  font-family: 'Lato', sans-serif;
+  font-weight: 400;
+  margin-bottom: 1rem;
+  text-align: center;
+}
+
+.pokedex-description {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  row-gap: 0.2rem;
+  margin-bottom: 1.5rem;
+}
+
+.pokedex-description p {
+  font-family: 'Lato', sans-serif;
+  font-weight: 500;
+  margin: 0;
+  color: #5e5e5e;
+}
+
+@media (min-width: 768px) {
+  .pokedex-container {
+    padding: 3rem;
+  }
+
+  .welcome-header {
+    margin-bottom: 1.5rem;
+    font-size: 1.5rem;
+  }
+
+  .pokedex-description {
+    row-gap: 0.5rem;
+  }
+
+  .pokedex-description p {
+    font-size: 1.125rem;
+  }
+}
+
+@media (min-width: 1024px) {
+  .pokedex-container {
+    padding: 4rem;
+  }
+
+  .welcome-header {
+    margin-bottom: 2rem;
+    font-size: 2rem;
+  }
+
+  .pokedex-description p {
+    font-size: 1.25rem;
+  }
+}
+</style>
